d941e8e3619a603fe5a54ac4f7d4d20b52d9fa7c
HenryCS <henrycsmith@proton.me>
2025-06-08 09:30:45 +0100

    Update Hero component and styles; add new ProductShowcase element and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Hero from './components/Hero'
+import ProductShowcase from './components/ProductShowcase'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Hero />
+      <ProductShowcase />
     </>
   )
 }
--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -2,8 +2,13 @@ import "./ProductShowcase.css";
 
 const ProductShowcase = () => {
   return (
-    <div className="hero-container">
-        
+    <div className="showcase-padding">
+        <div className="showcase-container">
+            <div className="showcase-content">
+                <h2>The 6-Lamp Ambient Set</h2>
+                <h2>Coming soon</h2>
+            </div>
+        </div>
     </div>
   );
 };
